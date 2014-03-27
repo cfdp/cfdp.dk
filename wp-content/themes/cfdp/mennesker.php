bb2a26a99767ac068336c39f931f9d7c0b3c75b0
Michael Lajlev <lajlev@gmail.com>
2014-03-27 13:00:25 +0100

    Daniel is back

--- wp-content/themes/cfdp/mennesker.php
+++ wp-content/themes/cfdp/mennesker.php
@@ -33,7 +33,7 @@ $count = 0;
 foreach ( $aUsersID as $iUserID ) :
 $user = get_userdata( $iUserID );
 
-$dont_show_these = array(8, 9, 14, 15, 16, 17, 19, 20, 25, 27, 28);
+$dont_show_these = array(8, 9, 14, 15, 16, 17, 19, 20, 25, 27);
 
 if( !in_array($user->ID, $dont_show_these) ) {
 
