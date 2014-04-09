69448381d3d5897ece9bc8f1a318dd88506af947
Michael Lajlev <lajlev@gmail.com>
2014-04-09 14:16:55 +0200

    added twitter feed to home

--- wp-content/themes/cfdp/home.php
+++ wp-content/themes/cfdp/home.php
@@ -16,13 +16,13 @@
   <div class="posts">
     <div class="left grid_8">
       <div class="latest">
-        <h2>Seneste indlæg</h2>
+        <h2>Blog indlæg</h2>
         <span class="line"></span>
 
 <?php //2 seneste indlæg med kategorien forsiden
         $query = array(
           'category__in' => array(9),
-          'posts_per_page'  => 3,
+          'posts_per_page'  => 4,
         );
         query_posts($query);
           if ( have_posts() ) : while ( have_posts() ) : the_post();
@@ -51,35 +51,13 @@
     </div> <!-- .left -->
 
     <div class="right grid_4">
+      <h2>Tweets</h2>
+      <span class="line"></span>
+      <a class="twitter-timeline" data-dnt="true" href="https://twitter.com/CFDPs" data-widget-id="453859743497322497" data-chrome="transparent noheader" data-tweet-limit="3">Tweets by @CFDPs</a>
+      <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 
-      <div class="news">
-        <h2>Nyheder fra centeret</h2>
-        <span class="line"></span>
-
-        <?php //3 seneste indlæg med kategorien nyheder fra CFDP
-        query_posts('cat=11&posts_per_page=1');
-          if ( have_posts() ) : while ( have_posts() ) : the_post();
-        ?>
-          <div class="post">
-            <h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
-            <span class="postInfo">
-              af <a href="#" class="author"><?php the_author_posts_link(); ?></a> for
-              <?php echo human_time_diff(get_the_time('U'), current_time('timestamp')) . ' siden'; ?>
-              <?php $comment_count = get_comment_count($post->ID); ?><?php if ($comment_count['approved'] > 0) : ?> | <?php endif; ?>
-              <?php comments_popup_link('', '1 Kommentar', '% Kommentarer'); ?>
-            </span>
-            <?php truncate(get_the_excerpt(), 350); ?> <a href="<?php the_permalink() ?>" class="more">Læs&nbsp;indlæg</a>
-          </div>
-
-        <?php endwhile; else:
-        echo('Desværre er der ingen nyheder fra Centeret');
-        endif;
-        wp_reset_query();
-        ?>
-
-        <a href="/kategori/nyheder/" class="readMore">Læs flere nyheder fra centeret</a>
-
-      </div>
+      <h2>Facebook venner</h2>
+      <span class="line"></span>
       <iframe id="facebook-faces" src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fcfdp.dk&amp;width=310&amp;height=258&amp;colorscheme=light&amp;show_faces=true&amp;header=false&amp;stream=false&amp;show_border=false&amp;appId=112787928922252" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:310px; height:258px;" allowTransparency="true"></iframe>
     </div><!-- .right -->
   </div>
