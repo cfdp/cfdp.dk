139e7cd657b5b054818e9d5cefe965a99c95aad2
Michael Lajlev <lajlev@gmail.com>
2016-08-05 16:59:48 +0200

    cleanup and removal of old theme

--- wp-content/themes/cfdp_1.0/inc/nav.php
+++
@@ -1,4 +0,0 @@
-<div class="navigation">
-	<div class="next-posts"><?php next_posts_link('&laquo; Tdiligere indl&aelig;g') ?></div>
-	<div class="prev-posts"><?php previous_posts_link('Nye indl&aelig;g &raquo;') ?></div>
-</div>
