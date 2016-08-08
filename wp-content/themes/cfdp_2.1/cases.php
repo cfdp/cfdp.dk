3c0c8d4c6b7781a69c681ffa1f16b9fc1a7a593f
Michael Lajlev <lajlev@gmail.com>
2016-08-08 21:22:28 +0200

    Simplified

--- wp-content/themes/cfdp_2.1/cases.php
+++ wp-content/themes/cfdp_2.1/cases.php
@@ -17,9 +17,6 @@ Template Name: Cases
 
 		<?php
 
-		// Projekter category = ID 7
-		$categories=get_categories('include=20,21&orderby=slug');
-
 		// post template
 		function postContent()
 		{
@@ -29,51 +26,41 @@ Template Name: Cases
 			echo ' <a href="'; the_permalink(); echo '" class="more">Læs mere</a>';
 			echo '</p>';
 		}
-		// array of child cat to cat ID 7 in a foreach
-		foreach($categories as $category)
-		{
-			if($category->category_description!='menu'){
-				echo '<div class="category-wrap">';
+		echo '<div class="category-wrap">';
 
-				// query all post in child cat
-				query_posts("posts_per_page=30&cat=$category->cat_ID&meta_key=order&orderby=meta_value_num&order=ASC");
-				// $count used to set css class if post a first or last (1,3,5,7 ect)
-				if ( have_posts() ) : $count = 0;
-						while ( have_posts() ) : the_post();
-						$count++;
-						if ($count % 2 == 0) : ?>
+		// query all posts with case category
+		query_posts("posts_per_page=30&cat=311&meta_key=order&orderby=meta_value_num&order=ASC");
+		if ( have_posts() ) : $count = 0;
+				while ( have_posts() ) : the_post();
+				$count++;
+				if ($count % 2 == 0) : ?>
 
-							<div class="grid_6 omega">
-								<div class="post">
-									<?php postContent() ?>
-								</div>
-							</div>
-
-						<?php else : ?>
-
-							<div class="grid_6 alpha">
-								<div class="post">
-									<?php postContent() ?>
-								</div>
-							</div>
-
-						<?php endif; ?>
-					<?php endwhile;
-					echo '
+					<div class="grid_6 omega">
+						<div class="post">
+							<?php postContent() ?>
+						</div>
 					</div>
-					<div class="grid_12 clearfix" style="margin-bottom:50px;">
-					</div>
-					</div><!--/category-->';
-					else:
-					echo('Desværre ingen indlæg fundet');
-					endif;
-					wp_reset_query();
-			}
-		}
-		wp_reset_query();
 
-		?>
-	</div>
+				<?php else : ?>
+
+					<div class="grid_6 alpha">
+						<div class="post">
+							<?php postContent() ?>
+						</div>
+					</div>
+
+				<?php endif; ?>
+			<?php endwhile;
+			echo '
+			</div>
+			</div><!--/category-->';
+			else:
+			echo('Desværre ingen indlæg fundet');
+			endif;
+			wp_reset_query();
+
+?>
+</div>
 
 
 <?php get_footer(); ?>
