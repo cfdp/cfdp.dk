9df9ca76dd6e5feb512b5ebaeed9fe6e4d771e0a
Michael Lajlev <lajlev@gmail.com>
2016-08-10 14:53:34 +0200

    cleanup

--- wp-content/themes/cfdp_2.1/searchform.php
+++ wp-content/themes/cfdp_2.1/searchform.php
@@ -3,11 +3,9 @@
 action="<?php bloginfo('home'); ?>/">
 <input
 	type="search"
-	value="<?php echo $search_text; ?>"
+  placeholder="<?php echo $search_text; ?>"
 	name="s"
 	id="s"
-	onblur="if (this.value == '') {this.value = '<?php echo $search_text; ?>';}"
-	onfocus="if (this.value == '<?php echo $search_text; ?>')  {this.value = '';}"
 />
 <input type="hidden" id="searchsubmit" />
 </form>
