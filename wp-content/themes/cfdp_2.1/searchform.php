085f1cd55c4c7d2ba8768a8eafe19082c6b4d93d
Michael Lajlev <lajlev@gmail.com>
2016-08-11 09:25:45 +0200

    Add English flag on desktop Improve searchbar visuals

--- wp-content/themes/cfdp_2.1/searchform.php
+++ wp-content/themes/cfdp_2.1/searchform.php
@@ -1,9 +1,8 @@
-<?php $search_text = "Søg på siden..."; ?>
 <form method="get" id="searchform"
 action="<?php bloginfo('home'); ?>/">
 <input
-	type="search"
-  placeholder="<?php echo $search_text; ?>"
+	type="text"
+  placeholder="Søg"
 	name="s"
 	id="s"
 />
