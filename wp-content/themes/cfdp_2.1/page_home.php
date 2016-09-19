3c47c6135f99a269c9b69b93e437d6637e3e199f
Michael Lajlev <lajlev@gmail.com>
2016-09-19 20:46:45 +0200

    Add page template: Home

---
+++ wp-content/themes/cfdp_2.1/page_home.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Template Name: Page Home
+*/
+?>
+<?php get_header(); ?>
+
+<div class="content clearfix">
+  <div class="grid_12 clearfix">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      <?php the_content(); ?>
+    <?php endwhile; endif; ?>
+  </div>
+
+  <div class="slider grid_12 clearfix">
+    <?php dynamic_content_gallery(); ?>
+  </div><!-- .slider -->
+  <div class="grid_12 clearfix">
+    <div class="blog-posts">
+      <h2 class="clearfix">Seneste indlæg</h2>
+      <?php
+        $query = array('category__in' => array(9), 'posts_per_page'  => 6, 'paged'  => $paged);
+        query_posts($query);
+        include (TEMPLATEPATH . '/inc/posts.php' );
+      ?>
+
+        <div class="paging">
+          <?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Tidligere indlæg', 'twentyten' ) ); ?>
+          <?php previous_posts_link( __( 'Nyere indlæg <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?>
+        </div>
+        <?php wp_reset_query(); ?>
+
+
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+<?php get_footer(); ?>
