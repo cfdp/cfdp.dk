2a4117568985a1468715500a35c2d1a5ba3c0bf7
Michael Lajlev <lajlev@gmail.com>
2016-10-01 09:25:59 +0200

    Better footer and facepile

--- wp-content/themes/cfdp_2.1/footer.php
+++ wp-content/themes/cfdp_2.1/footer.php
@@ -5,26 +5,24 @@
   <div class="footer container wrap clearfix">
     <div class="grid_12 zi1">
 
-    	<div class="leftFoot grid_8 alpha">
-    		<span class="line"></span>
-    		<p class="grid_4 alpha">
-    			<strong>Center for Digital Pædagogik</strong><br>
+    	<div class="leftFoot grid_6 alpha">
+    		<h3>Center for Digital Pædagogik</h3>
+    		<p class="grid_3 alpha">
     			Skanderborgvej 156<br>
     			8260 Viby J.<br>
-    			Danmark<br>
+    			Danmark
     		</p>
-    		<p class="grid_4 omega clearfix">
-          <b>Kontakt os</b><br>
-    			Email <a href="mailto:info@cfdp.dk">info@cfdp.dk</a> <br />
-    			Telefon 86 370 400<br />
-    			CVR nr 37 25 17 71
+    		<p class="grid_3 omega clearfix">
+    			Email: <a href="mailto:info@cfdp.dk">info@cfdp.dk</a><br>
+    			Telefon: 86 370 400<br>
+    			CVR nr.: 37 25 17 71
     		</p>
     	</div>
     	<!-- Right footer | Social Links -->
-    	<div class="foot grid_4 omega clearfix">
+    	<div class="foot grid_6 omega clearfix">
     	  <div class="inner">
-          <p><b>Facebook</b></p>
-           <div class="fb-like" data-href="https://www.facebook.com/cfdp.dk" data-width="310" data-layout="standard" data-action="like" data-size="large" data-show-faces="true" data-share="true"></div>
+          <h3>Facebook</h3>
+           <div class="fb-like" data-href="https://www.facebook.com/cfdp.dk" data-width="480" data-layout="standard" data-action="like" data-size="large" data-show-faces="true" data-share="false" data-colorscheme="dark"></div>
         </div>
       </div>
 
