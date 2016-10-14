93566a4ccf67ad718a0d4884620dae030b138b57
Michael Lajlev <lajlev@gmail.com>
2016-10-14 12:37:48 +0200

    visualfix

--- wp-content/themes/cfdp_2.1/international.php
+++ wp-content/themes/cfdp_2.1/international.php
@@ -1,48 +1,47 @@
 <?php
 /*
-Template Name: International
+Template Name: Page Home
 */
 ?>
 <?php get_header(); ?>
-<div class="international content grid_12 clearfix">
-	<div class="posts grid_6 alpha zi1">
-		<h2>International news</h2>
 
-		<span class="line"></span>
-		<?php
-		$query = array(
-		'category__in' => array(35),
-		'posts_per_page' => 6,
-		);
-		query_posts($query);
+<div class="content clearfix">
+  <div class="grid_12 clearfix">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      <?php the_content(); ?>
+    <?php endwhile; endif; ?>
+  </div>
+  <div class="grid_12 clearfix">
+    <div class="blog-posts">
+      <?php
+        $paged = ( get_query_var('page') ) ? get_query_var('page') : 1;
 
-		if ( have_posts() ) : while ( have_posts() ) : the_post();
-		?>
-			<div class="post">
-				<h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
-				<span class="postInfo">written by <a href="#" class="author"><?php the_author_posts_link(); ?></a> | <?php comments_popup_link('Leave a comment', '1 Comment', '% Comments'); ?></span>
-				<?php truncate(get_the_excerpt(), 350); ?> <a href="<?php the_permalink() ?>" class="more">read more</a>
-			</div><!-- end .post -->
-		<?php
-		endwhile; else:
-			echo('Desværre ingen indlæg fundet');
-		endif;
+        $query = array(
+          'category__in' => array(35),
+          'posts_per_page'  => 6,
+          'paged'  => $paged
+        );
+        query_posts($query);
+        if ( have_posts() ) : while ( have_posts() ) : the_post();
+        include (TEMPLATEPATH . '/inc/post.php' );
+        endwhile; else:
+        echo('Desværre, ingen indlæg fundet');
+        endif;
+       ?>
 
-		wp_reset_query();
-		?>
+        <div class="paging">
+          <?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Tidligere indlæg', 'twentyten' ) ); ?>
+          <?php previous_posts_link( __( 'Nyere indlæg <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?>
+        </div>
+        <?php wp_reset_query(); ?>
 
-		<a href="/kategori/news/" class="readMore">Read more news in english</a>
-	</div><!-- end .posts -->
 
-	<div class="about posts grid_6 omega clearfix zi1">
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<h2><?php the_title(); ?></h2>
-		<span class="line"></span>
-		<?php the_content(); ?>
-
-		<?php endwhile; endif; ?>
-	</div><!-- end .about posts -->
-
+      </div>
+    </div>
+  </div>
 </div>
+
+
+
 
 <?php get_footer(); ?>
