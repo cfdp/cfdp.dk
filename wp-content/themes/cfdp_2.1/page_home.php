fce8c321c72c4e2bbc770c84ae196c9e60f3bf11
Jonas Iversen <ji@peytz.dk>
2018-06-12 10:34:09 +0200

    show video from custom field values in frontpage template file

--- wp-content/themes/cfdp_2.1/page_home.php
+++ wp-content/themes/cfdp_2.1/page_home.php
@@ -4,6 +4,24 @@ Template Name: Page Home
 */
 ?>
 <?php get_header(); ?>
+
+<?php
+
+// Get the video URL and put it in the $video variable
+$videoID = get_post_meta($post->ID, 'banner_video', true);
+if ($videoID) {
+	$videoURL = 'http://www.youtube.com/watch?v=' . $videoID; 
+}
+?>
+
+<section id="big-video">
+    <div class="video" 
+         data-src="<?php the_field('fallback_img') ?>" 
+         data-video="<?php echo $videoURL; ?>" 
+         data-placeholder="<?php the_field('start_img') ?>">
+    </div>
+</section>
+
 <div class="content clearfix">
   <div class="grid_12 clearfix">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
