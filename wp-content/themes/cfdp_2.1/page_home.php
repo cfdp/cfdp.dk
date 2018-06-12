a845aa1fb1f8b12f262a8b474ff7f400405b28c5
Jonas Iversen <ji@peytz.dk>
2018-06-12 09:36:45 +0200

    deleted auto post loop in template file

--- wp-content/themes/cfdp_2.1/page_home.php
+++ wp-content/themes/cfdp_2.1/page_home.php
@@ -10,26 +10,6 @@ Template Name: Page Home
       <?php the_content(); ?>
     <?php endwhile; endif; ?>
   </div>
-  <div class="grid_12 clearfix">
-    <div class="blog-posts">
-      <?php
-        $paged = ( get_query_var('page') ) ? get_query_var('page') : 1;
-        $query = array(
-          'category__in' => array(9),
-          'posts_per_page'  => 6,
-          'paged'  => $paged
-        );
-        query_posts($query);
-        include (TEMPLATEPATH . '/inc/posts.php' );
-      ?>
-        <div class="paging">
-          <?php previous_posts_link( __( '<span class="meta-nav">&larr;</span> Nyere indlæg', 'twentyten' ) ); ?>
-          <?php next_posts_link( __( 'Tidligere indlæg <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?>
-
-        </div>
-        <?php wp_reset_query(); ?>
-      </div>
-    </div>
   </div>
 </div>
 <?php get_footer(); ?>
