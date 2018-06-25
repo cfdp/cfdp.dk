ad8677465c3019de80ca74c28291df017ad2565f
Jonas Iversen <ji@peytz.dk>
2018-06-25 09:21:08 +0200

    css

--- wp-content/themes/cfdp_2.1/author.php
+++ wp-content/themes/cfdp_2.1/author.php
@@ -3,10 +3,10 @@
 	$curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
 ?>
 <div class="content grid_12 clearfix">
-
-	<h1 class="name"><?php echo $curauth->display_name; ?></h1>
+    
+    <p class="sub-titel">Seneste indlæg</p>
+	<h2 class="name"><?php echo $curauth->display_name; ?></h2>
 	<div class="blog-posts">
-		<h2>Seneste indlæg</h2>
 		<?php query_posts($query_string . '&cat=10&posts_per_page=5'); ?>
 			<?php include (TEMPLATEPATH . '/inc/posts.php' ); ?>
 			<div class="paging"><?php wp_pagenavi(); ?></div>
