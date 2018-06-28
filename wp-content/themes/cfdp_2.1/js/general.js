8313aea0ff3e21dba4a5f44ef43d2cd82bb64ee0
Jonas Iversen <ji@peytz.dk>
2018-06-28 13:56:52 +0200

    menu changes

--- wp-content/themes/cfdp_2.1/js/general.js
+++ wp-content/themes/cfdp_2.1/js/general.js
@@ -48,15 +48,19 @@ $(document).mouseup(function (e){
     }
 });  	
 
+    
+
 
     
-    
-    
+var $newDiv = $("<div/>")   // creates a div element
+    .addClass("menu_arrow_holder")   // add a class
+    .html("<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 11 19'><path class='menu_arrow' d='M10.5,8.2L3,0.5c-0.7-0.7-1.8-0.7-2.5,0c-0.7,0.7-0.7,1.9,0,2.6l6.2,6.4l-6.2,6.4c-0.7,0.7-0.7,1.9,0,2.6c0.7,0.7,1.8,0.7,2.5,0l7.5-7.7C11.2,10.1,11.2,8.9,10.5,8.2z'/></svg>");    
+$( "#cssmenu > ul > li:has(ul)" ).append($newDiv);    
     
 $('.current_page_item').closest('.menu-item-has-children').addClass("opened");    
     
-$('#cssmenu > ul > li > a').click(function() {
-    var checkElement = $(this).next();	
+$('#cssmenu > ul > li > .menu_arrow_holder').click(function() {
+    var checkElement = $(this).prev();	
 
     if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
       $(this).closest('li').removeClass('active');
